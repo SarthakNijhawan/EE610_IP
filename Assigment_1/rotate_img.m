3cab94976d830c9e6263fbdae6a72d78e239bcb9
harshit1425 <42450770+harshit1425@users.noreply.github.com>
2018-08-18 20:24:56 +0530

    Add files via upload

--- Assigment_1/rotate_img.m
+++ Assigment_1/rotate_img.m
@@ -1,28 +1,27 @@
 function [rotated_img] = rotate_img(im,theta)
-img = im;
-A=imread(img);
+A = im;
+%A=imread(img);
 
-%new image empty matrix
 rotated_img = zeros(size(A));
 
 %Specify the degree
-deg=theta;
+rad = theta*pi/180;
 
-%changing the origin
-nRow = size(A,1);
-nCol = size(A,2);
-half_nRow = nRow./2;
-half_nCol = nCol./2;
+midx = size(A,1);
+midy = size(A,2);
 
-for i=1:size(rotated_img,1)
-    for j=1:size(rotated_img,2)
-         x= (i)*cos(deg)+(j)*sin(deg);
-         y=-(i)*sin(deg)+(j)*cos(deg);
-         x= round(x)+half_nRow;
-         y= round(y)+half_nCol;
+for i=1:size(A,1)
+    for j=1:size(A,2)
 
-         if (x >= 1 && y >= 1 && x <= size(A,2) && y <= size(A,1))
-              rotated_img(i,j) = A(x,y); % k degrees rotated image         
+         x=(i)*cos(rad)+(j)*sin(rad);
+         y= -(i)*sin(rad)+(j)*cos(rad);
+         x=round(x)+midx;
+         y=round(y)+midy;
+
+         if (x>=1 && y>=1 && x<=size(A,2) && y<=size(A,1))
+              rotated_img(x,y)=A(i,j);          
          end
     end
 end
+
+imshow(rotated_img);
