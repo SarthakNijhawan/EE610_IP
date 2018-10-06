ab931015f668507ba15e2a8b0b9d8db9f6e2f3a9
SarthakNijhawan <sarthakn69@gmail.com>
2018-10-06 10:32:59 +0530

    Added assignment 2

---
+++ Assignment_2/load_images.m
@@ -0,0 +1,25 @@
+einstein = imread('images/einstein.jpg');
+barbara = imread('images/barbara.png');
+lena = imread('images/lena.png');
+baboon = imread('images/baboon.png');
+photo = imread('images/photo.jpeg');
+goldhill = imread('images/goldhill.bmp');
+hist_eq = imread('images/histo_eq.jpg');
+hist_eq3 = imread('images/hist_eq3.jpg');
+sharp = imread('images/sharp.png');
+% sharp_bw = rgb2gray(sharp);
+
+lena_bw = rgb2gray(lena);
+einstein_bw = rgb2gray(einstein);
+
+adap_thresh2 = imread('images/adap_thresh2.png');
+adap_thresh2 = rgb2gray(adap_thresh2);
+adap_thresh3 = imread('images/adap_thresh3.png');
+% adap_thresh3 = rgb2gray(adap_thresh3);
+
+adap_thresh4 = imread('images/adap_thresh4.jpg');
+
+dft_im = imread('images/dft1.png');
+dft_im = rgb2gray(dft_im);
+
+dft_im2 = imread('images/dft2.png');
