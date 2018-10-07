58163f3fc31e569770e2c481b36179d8a16340b4
SarthakNijhawan <sarthakn69@gmail.com>
2018-10-07 20:48:58 +0530

    Code working for grayscale images

--- Assignment_2/trial.m
+++ Assignment_2/trial.m
@@ -1,26 +1,38 @@
-img = 
+% img = imread('images/Blurry1_1.jpg');
+% kernel = imread('images/kernel1_tiled.png');
+
 h = fspecial('disk', 15);
 
-% Read image and convert to double for FFT
 cam = im2double(rgb2gray(imread('images/lena_bw.png')));
-hf = fft2(h,size(cam,1),size(cam,2));
-
+hf = fft2(h, size(cam,1), size(cam,2));
 cam_blur = real(ifft2(hf.*fft2(cam)));
-
-figure
-subplot(141)
-imshow(cam_blur)
-
 sigma_u = 10^(-40/20)*abs(1-0);
-cam_blur_noise = cam_blur + sigma_u*randn(size(cam_blur));
-subplot(142)
-imshow(cam_blur_noise)
+img = cam_blur + sigma_u*randn(size(cam_blur));
 
-cam_inv = inverse_filtering(cam_blur_noise, h, 0.1);
-subplot(143)
-imshow(cam_inv)
+restore_img(img, h, 'clsf', 0.01);
 
-cam_pinv = real(ifft2((abs(hf) > 0.1).*fft2(cam_blur_noise)./hf));
-subplot(144)
-imshow(cam_pinv)
-xlabel('pseudo-inverse restoration')
+% h = fspecial('disk', 15);
+
+% % Read image and convert to double for FFT
+% cam = im2double(rgb2gray(imread('images/lena_bw.png')));
+% hf = fft2(h,size(cam,1),size(cam,2));
+
+% cam_blur = real(ifft2(hf.*fft2(cam)));
+
+% figure
+% subplot(141)
+% imshow(cam_blur)
+
+% sigma_u = 10^(-40/20)*abs(1-0);
+% cam_blur_noise = cam_blur + sigma_u*randn(size(cam_blur));
+% subplot(142)
+% imshow(cam_blur_noise)
+
+% cam_inv = inverse_filtering(cam_blur_noise, h, 0.1);
+% subplot(143)
+% imshow(cam_inv)
+
+% cam_pinv = real(ifft2((abs(hf) > 0.1).*fft2(cam_blur_noise)./hf));
+% subplot(144)
+% imshow(cam_pinv)
+% xlabel('pseudo-inverse restoration')
