fafda82eff7e60037d73f56c4b0dca733f1021b2
SarthakNijhawan <sarthakn69@gmail.com>
2018-11-25 02:22:37 +0530

    New files added

---
+++ Sudoku_Solver_Project/main.py
@@ -0,0 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import cv2 as cv
+
+from os.path import join
+
+
+####################################### MAIN FUNCTION ########################################
+def main():
+	#Read the image from the camera
+
+	#Pre-processing on the image
+
+	#
+
+if __name__ == '__main__':
+	main()
